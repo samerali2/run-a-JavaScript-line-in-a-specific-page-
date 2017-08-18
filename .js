311f5c805f74a1ebae144abc5a997887353a8325
Sam <samerali2@gmail.com>
2017-08-18 20:40:28 +0200

    Update and rename .gitignore to .js

---
+++ .js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         - it's
+// @namespace    https://sumerian.me/
+// @version      0.1
+// @description  no education!
+// @author       Sam
+// @match        https://hib.itslearning.com/index.aspx?Username=&RedirectLogin=hvl.itslearning.com
+// @match        https://hib.itslearning.com/Index.aspx
+// @match        https://hib.itslearning.com/
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    // your line next!
+
+	__doPostBack('ctl00$ContentPlaceHolder1$federatedLoginButtons$ctl00$ctl00','')
+
+})();
